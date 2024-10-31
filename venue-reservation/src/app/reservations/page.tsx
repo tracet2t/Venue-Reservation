ee94b6ab89dbe9aa9b976f20d3ed7305cd34963f
VIHARI7 <dviha7@gmail.com>
2024-11-01 00:12:13 +0530

    style: improve layout responsiveness for filters

--- venue-reservation/src/app/reservations/page.tsx
+++ venue-reservation/src/app/reservations/page.tsx
@@ -100,14 +100,24 @@ export const  Reservations= () => {
     };
   
     return (
-      <div className="flex flex-col sm:flex-col md:flex-row items-center gap-4 p-4 bg-white border rounded-lg shadow-lg max-w-[1400px] mx-auto space-y-4 space-x-0 md:space-x-8">
+      <div className="flex flex-wrap items-center gap-4 p-4 bg-white border rounded-lg shadow-lg max-w-[1400px] mx-auto space-y-0 space-x-0 md:space-x-8 ">
         {/* Location Filter */}
-        <div className="relative z-10 w-full sm:w-full md:w-auto ml-0 md:ml-12">
+        <div className="relative z-10 w-full sm:w-auto md:w-auto ml-0 md:ml-12">
         <button
           onClick={toggleLocationDropdown}
-          className="px-4 py-2 border rounded-lg w-full sm:w-full md:w-72 bg-white focus:outline-none focus:ring-2 focus:ring-[#584822]"
+          className="flex items-center justify-between gap-6 px-4 py-2 border rounded-lg w-full sm:w-full md:w-72 bg-white focus:outline-none focus:ring-2 focus:ring-[#584822]"
         >
-          Location
+          <img
+            src="https://img.icons8.com/ios/50/marker--v1.png"
+            alt="Location Icon"
+            className="w-5 h-5" 
+            />
+           <span style={{ color: "#584822" }}>Location </span>
+          <img
+              src="https://img.icons8.com/ios/50/sort-down.png"
+              alt="Dropdown Icon"
+              className="w-4 h-4"
+             />
         </button>
         {isLocationDropdownOpen && (
           <div className="absolute mt-2 w-72 bg-white border rounded-lg shadow-lg p-4">
@@ -149,9 +159,19 @@ export const  Reservations= () => {
         <div className="relative z-10 w-full sm:w-full md:w-auto ml-0 md:ml-12">
           <button
             onClick={toggleVenueDropdown}
-            className="px-4 py-2 border rounded-lg w-full sm:w-full md:w-72 bg-white focus:outline-none focus:ring-2 focus:ring-[#584822]"
+            className="flex items-center justify-between gap-6 px-4 py-2 border rounded-lg w-full sm:w-full md:w-72 bg-white focus:outline-none focus:ring-2 focus:ring-[#584822]"
           >
-            Venue Type
+            <img
+            src="https://img.icons8.com/ios/50/performance.png"
+            alt="Location Icon"
+            className="w-6 h-6" 
+          />
+           <span style={{ color: "#584822" }}> Venue Type</span>
+            <img
+              src="https://img.icons8.com/ios/50/sort-down.png"
+              alt="Dropdown Icon"
+              className="w-4 h-4"
+            />
           </button>
           {isVenueDropdownOpen && (
             <div className="absolute mt-2 w-72 bg-white border rounded-lg shadow-lg p-2">
@@ -198,12 +218,22 @@ export const  Reservations= () => {
         </div>
   
         {/* Venue Name Input */}
+       <div className="relative w-full sm:w-full md:w-72">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="20" 
+          height="20"
+          viewBox="0 0 50 50"
+          className="absolute left-3 top-1/2 transform -translate-y-1/2 text-[#584822]"
+        >
+          <path d="M 21 3 C 11.621094 3 4 10.621094 4 20 C 4 29.378906 11.621094 37 21 37 C 24.710938 37 28.140625 35.804688 30.9375 33.78125 L 44.09375 46.90625 L 46.90625 44.09375 L 33.90625 31.0625 C 36.460938 28.085938 38 24.222656 38 20 C 38 10.621094 30.378906 3 21 3 Z M 21 5 C 29.296875 5 36 11.703125 36 20 C 36 28.296875 29.296875 35 21 35 C 12.703125 35 6 28.296875 6 20 C 6 11.703125 12.703125 5 21 5 Z"></path>
+        </svg>
         <input
           type="text"
-          placeholder="Venue name"
-          className="px-4 py-2 border rounded-lg w-full sm:w-full md:w-72 bg-white focus:outline-none focus:ring-2 focus:ring-[#584822]"
+          placeholder="Venue Name"
+          className="px-12 py-2 text-center border rounded-lg w-full sm:w-full md:w-72 bg-white focus:outline-none focus:ring-2 focus:ring-[#584822]"
         />
-  
+      </div>
         {/* Search Button */}
         <button className="px-6 py-2 w-full sm:w-full md:w-64 text-white bg-[#584822] rounded-lg hover:bg-[#7b5e34] focus:outline-none">
           Search
