7938d590c390e078fe2f5bce5210ec251ed51362
LakshananS <149660628+LakshananS@users.noreply.github.com>
2024-11-01 12:09:00 +0530

    86b2kuqe7-database-creations: Separate Database Project and Generate Prisma Client

--- venue-reservation/src/app/reservations/page.tsx
+++ venue-reservation/src/app/reservations/page.tsx
@@ -1,5 +1,27 @@
-export default function Reservations() {
-    return (
-        <div>Reservations page</div>
-    );
-}
+// Reservation.tsx
+'use client'
+import VenueCard from '@/components/venue_card/user_venue_card';
+import Header from '@/app/layouts/Header';
+import Footer from '@/app/layouts/Footer';
+
+const Reservation = () => {
+  return (
+    <div className="min-h-screen bg-gray-100 flex flex-col justify-between">
+      {/* Header */}
+      <Header />
+
+      {/* Main Content */}
+      <main className="flex-grow p-8">
+        
+
+        {/* Venue Card */}
+        <VenueCard />
+      </main>
+
+      {/* Footer */}
+      
+      <Footer />
+    </div>
+  );
+};
+export default Reservation;
--- venue-reservation/tailwind.config.ts
+++ venue-reservation/tailwind.config.ts
@@ -15,6 +15,9 @@ const config: Config = {
       borderRadius: {
         'xl': '28px', 
       },
+      zIndex: {
+        '1': '1',
+      },
     },
   },
   plugins: [],
