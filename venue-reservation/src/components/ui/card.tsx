761ee2ba65d95aa68498df3d47b90d069c54ed8c
Aynul Amna <amnabinthaman@gmail.com>
2024-11-07 14:36:06 +0530

    86b2rcabb:updated Card

--- venue-reservation/src/components/ui/card.tsx
+++ venue-reservation/src/components/ui/card.tsx
@@ -1,14 +1,12 @@
-// src/components/ui/Card.tsx
-
 import React from "react";
 
 interface CardProps {
-  children: React.ReactNode; // This allows the card to contain any content passed to it
+  children: React.ReactNode;
 }
 
 const Card: React.FC<CardProps> = ({ children }) => {
   return (
-    <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-md">
+    <div className="bg-white p-8 md:p-12 lg:p-16 rounded-3xl shadow-lg w-full  h-[450px]  flex flex-col justify-center">
       {children}
     </div>
   );
