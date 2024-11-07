434b6d01d5bb097992a98da22a2efdef6b822414
Aynul Amna <amnabinthaman@gmail.com>
2024-11-07 23:16:53 +0530

    86b2rcabb:Created component for branding section

--- venue-reservation/src/app/signup-landing/page.tsx
+++ venue-reservation/src/app/signup-landing/page.tsx
@@ -1,25 +1,15 @@
 import React from "react";
 import Card from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
-
+import BrandingSection from "@/components/branding-section";
 const SignupLanding = () => {
   return (
     <div className="flex flex-col min-h-screen bg-gray-100 items-center justify-center">
       <div className="flex flex-col lg:flex-row w-full max-w-5xl items-center lg:items-stretch justify-between p-6 lg:p-12 space-y-6 lg:space-y-0 lg:space-x-12">
         
         {/* Left Branding Section  */}
-        <div className="bg-gradient-to-b from-[#312405] via-[#735203] via-[#78580E] to-[#906B11] text-white p-10 md:p-14 lg:p-20 rounded-3xl shadow-lg w-5/6 max-w-xs lg:max-w-md h-[600px] flex flex-col justify-between">
-        <div>
-          <h1 className="text-4xl font-bold">RS.</h1>                                                                                     
-          <h2 className="text-lg font-semibold mt-4">Reservation System</h2>
-        </div>
-        <div>
-          <p className="mt-8 text-base leading-relaxed">
-            “Effortlessly book and manage your event with our seamless reservation system, designed for convenience and flexibility.”
-          </p>
-        </div>
-      </div>
-
+        <BrandingSection />
+        
         {/* Right Sign-Up Section */}
         <div className="flex flex-1 items-center justify-center">
           <Card>
